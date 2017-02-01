d6e7861d4316f76673e5e198d3fe07c5751ec1d2
AnnMarie <AnnMarieTorres@outlook.com>
2017-01-31 19:20:52 -0500

    html css complete

---
+++ script.js
@@ -0,0 +1,75 @@
+var total=0;
+var str;
+function acClick(){
+	total = 0;
+	document.getElementById("screen").value=0;
+	window.alert("ac");
+}
+function ceClick(){
+	//remove last option
+	window.alert("ce");
+}
+function divClick(){
+	//divide
+	window.alert("divide");
+}
+function multClick(){
+	//multiply
+	window.alert("multiply");
+}
+function minusClick(){
+	//minus
+	window.alert("minus");
+}
+function plusClick(){
+	//plus
+	window.alert("plus");
+}
+function equalClick(){
+	//equal
+	window.alert("equal");
+}
+function decimalClick(){
+	//.
+	window.alert("decimal");
+}
+function nineClick(){
+	//9
+	window.alert("9");
+}
+function eightClick(){
+	//8
+	window.alert("8");
+}
+function sevenClick(){
+	//7
+	window.alert("7");
+}
+function sixClick(){
+	//6
+	window.alert("6");
+}
+function fiveClick(){
+	//5
+	window.alert("5");
+}
+function fourClick(){
+	//4
+	window.alert("4");
+}
+function threeClick(){
+	//3
+	window.alert("3");
+}
+function twoClick(){
+	//2
+	window.alert("2");
+}
+function oneClick(){
+	//1
+	window.alert("1");
+}
+function zeroClick(){
+	//0
+	window.alert("0");
+}
